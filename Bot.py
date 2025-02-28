22fb3386dfb13308b268a1a77eea0103e5bd704a
2005Yaswi <149513680+2005Yaswi@users.noreply.github.com>
2025-02-28 14:44:04 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -20,7 +20,7 @@ df = df.fillna("")
 df['Question'] = df['Question'].str.lower()
 df['Answer'] = df['Answer'].str.lower()
 
-vectorizer = TfidVectorizer()
+vectorizer = TfidfVectorizer()
 question_vectors = vectorizer.fit_transform(df['Question'])
 
 API_KEY = "AIzaSyDTf83tvdHXWC3KvZG9vyMb5-7k9Pkh4Vs" 
